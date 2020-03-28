2edf442d7e9b7fd6156610fb3910645c74c99818
KatarzynaWolska <kasia.wolska137@gmail.com>
2020-03-28 22:17:29 +0100

    Version with intersection

--- display_module/display.py
+++ display_module/display.py
@@ -68,8 +68,6 @@ class MainWindow(QMainWindow):
 
 
     def project_point(self, x, y, z):
-        if z < 0:
-            z = 0.000001
         new_x = x * (self.distance / z)
         new_y = y * (self.distance / z)
         return QPointF(new_x, new_y)
@@ -82,15 +80,97 @@ class MainWindow(QMainWindow):
             else:
                 self.painter.drawLine(points[i], points[i+1])
 
+    
+    def draw_rectangle2(self, points):
+        for line in points:
+            self.painter.drawLine(line[0], line[1])
+
 
     def projection(self):
         polygons = self.matrix_trans.get_polygons()
-        for polygon in polygons:
+        """for polygon in polygons:
             points = []
             for coords in polygon:
                 projected_point = self.project_point(coords[0], coords[1], coords[2])
                 if projected_point != None:
                     points.append(self.project_point(coords[0], coords[1], coords[2]))
 
-            self.draw_rectangle(points)
+            self.draw_rectangle(points)"""
+
+        for j in range(0, len(polygons)):
+            points = []
+            coords = polygons[j]
+            for i in range(0, len(coords)):
+                point1 = coords[i]
+                if i == (len(coords) - 1):
+                    point2 = coords[0]
+                else:
+                    point2 = coords[i+1]
+
+                res = self.check_points(point1, point2)    
+
+                if res != None:
+                    points.append(res)
+
+            self.draw_rectangle2(points)
+
+
+    def check_points(self, point1, point2):
+        if point1[2] <= 1 and point2[2] <= 1:
+            return None
+        
+        res = None
+
+        if point1[2] <= 1:
+            direction = numpy.array([
+                point2[0] - point1[0],
+                point2[1] - point1[1],
+                point2[2] - point1[2]
+            ])
+
+            res = self.intersection(direction, point1)
+            
+            proj_point1 = self.project_point(res[0], res[1], res[2])
+            proj_point2 = self.project_point(point2[0], point2[1], point2[2])
+
+        elif point2[2] <= 1:
+            direction = numpy.array([
+                point1[0] - point2[0],
+                point1[1] - point2[1],
+                point1[2] - point2[2]
+            ])
+
+            res = self.intersection(direction, point2)
+
+            proj_point1 = self.project_point(point1[0], point1[1], point1[2])
+            proj_point2 = self.project_point(res[0], res[1], res[2])
+
+        else:    
+            proj_point1 = self.project_point(point1[0], point1[1], point1[2])
+            proj_point2 = self.project_point(point2[0], point2[1], point2[2])
+
+
+        return (proj_point1, proj_point2)
+
+
+    def intersection(self, direction, point):
+        epsilon=1e-6
+        planeNormal = numpy.array([0, 0, 1])
+        planePoint = numpy.array([0, 0, 1]) #Any point on the plane
+
+        rayPoint = numpy.array([point[0], point[1], point[2]]) #Any point along the ray
+
+
+        ndotu = planeNormal.dot(direction) 
+
+        if abs(ndotu) < epsilon:
+            #print ("no intersection or line is within plane")
+            return None
+
+        w = rayPoint - planePoint
+        si = -planeNormal.dot(w) / ndotu
+        Psi = w + si * direction + planePoint
+
+        #print ("intersection at", Psi)
+        return Psi
 
